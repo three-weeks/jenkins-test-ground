c16ef16beeb16a17bf9ab6807f5cca8efa4a0eed
Kyle A. Matheny <kamathen@us.ibm.com>
2017-03-15 15:53:23 -0400

    add. groovy ext

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,45 @@
+pipeline {
+  agent any
+
+  environment {
+    gitUrl = ''
+    repo = ''
+  }
+
+  stages {
+    stage('Setup Environment') {
+      steps {
+        load './setup.groovy'
+      }
+    }
+
+    stage('Dev') {
+      steps {
+        load './dev.groovy'
+      }
+    }
+
+    stage('Test') {
+      steps {
+        load './test.groovy'
+      }
+    }
+
+    stage('UAT') {
+      steps {
+        load './uat.groovy'
+      }
+    }
+
+    stage('Prod') {
+      steps {
+        load './prod.groovy'
+      }
+    }
+  }
+  post {
+    always {
+      echo 'Done!'
+    }
+  }
+}
