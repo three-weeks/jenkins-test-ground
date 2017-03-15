efc95f2fd486f635bad2d5bd4990e75b545eb77f
Kyle A. Matheny <kamathen@us.ibm.com>
2017-03-15 15:33:53 -0400

    fix. relative to absolute path

--- dev.groovy
+++ dev.groovy
@@ -1,5 +1,5 @@
-build job: 'amkirsch/chef-pipeline/dev/cookbook-unit-tests',
+build job: '/amkirsch/chef-pipeline/dev/cookbook-unit-tests',
   parameters: [string(name: 'GIT_URL', value: gitUrl),
                string(name: 'COOKBOOK', value: gitRepo),
                string(name: 'GIT_COMMIT', value: gitCommit),
-               string(name: 'GIT_BRANCH', value: env.BRANCH_NAME)] 
+               string(name: 'GIT_BRANCH', value: env.BRANCH_NAME)]
